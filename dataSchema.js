74a40df72ee3347acb0292f597bc8ff083f5d84c
FlairMTech <129142458+FlairMTech@users.noreply.github.com>
2023-05-18 18:58:58 +0530

    Create dataSchema.js

---
+++ dataSchema.js
@@ -0,0 +1,38 @@
+
+const mongoose = require('mongoose');
+
+
+
+const categorySchema = new mongoose.Schema({
+  uniqueId: { type: Number, required: true, unique: true, default: 100  },
+  address: String,
+  date: String,
+  name: String,
+  items: [
+    {
+      category: String,
+      categoryPrice: String,
+      properties: [
+        {
+          subCategory: String,
+          length: Number,
+          width: Number,
+          unitPrice: Number,
+          totalSqft: Number,
+          totalPrice: Number
+        }
+      ]
+    }
+  ]
+});
+
+
+
+const dataSchema = new mongoose.Schema([categorySchema]);
+
+
+// Create the model
+const Data = mongoose.model('Data', dataSchema);
+
+// Export the model
+module.exports = Data;
